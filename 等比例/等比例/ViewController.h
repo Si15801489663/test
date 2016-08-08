a51a687673b3c9922d9a12421ed9cc5bbab56d61
YZTC-司东亮 <15801489663@163.com>
2016-08-08 15:41:18 +0800

    [text]  适配

---
+++ 等比例/等比例/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  等比例
+//
+//  Created by Macintosh HD on 16/7/13.
+//  Copyright © 2016年 Macintosh HD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
